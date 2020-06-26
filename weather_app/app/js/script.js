d37b362edf02e0f8dc6aec63d036c8cbbd3e356c
anna shchapina <shchapinaa@gmail.com>
2020-06-26 15:39:58 +0300

    create main sections, add owl-carousel

---
+++ weather_app/app/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+});
+
+
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items:3,
+    loop:true,
+    autoWidth: true,
+});
